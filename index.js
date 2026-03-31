120a75d3118f01738b3ab57dd9537b373bd21fdd
coder <ruisost01@gmail.com>
2026-03-31 17:25:49 +0100

    primeiro commit

---
+++ index.js
@@ -0,0 +1,42 @@
+//event com target
+/*
+document.querySelector("ul").addEventListener("click", function(e){
+    if(e.target.tagName=== 'LI'){
+        e.target.style.color="blue"
+    }
+})
+    */
+
+const entrada1= document.getElementById("entrada1");
+const entrada2= document.getElementById("entrada2");
+const mensagem= document.getElementById("sms");
+const mostrar= document.getElementById("show");
+entrada1.addEventListener("input", (e)=>{
+    dig= e.target.value;
+    mostrar.addEventListener("click", ()=>{
+
+        mensagem.textContent=dig;
+    })
+})
+
+
+/*
+function LimparCampos(){
+ entrada1.value=" ";
+  entrada2.value=" ";
+
+}
+
+
+
+const formulario= document.getElementById("form");
+
+formulario.addEventListener("submit",function(e){
+    e.preventDefault();
+    LimparCampos();
+    
+    alert("formulario enviado")
+
+})
+
+*/
